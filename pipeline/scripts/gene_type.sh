f243fa79109763645cc594e034644b7ab0736aa9
Alexis Morrissey <anm5579@psu.edu>
2025-06-03 13:37:47 -0400

    script cleanup

--- pipeline/scripts/gene_type.sh
+++ pipeline/scripts/gene_type.sh
@@ -3,5 +3,6 @@ bed_file="$1"
 output_file="$3"
 
 bedtools intersect -c -a "$bed_file" -b "$bam_file" > temp
-cat scripts/fo_header.txt <(sed 's/:/\t/g' temp | awk -v OFS='\t' '{print $4,$14}' | awk -F'\t' '{count[$1] += $2; total += $2} END {for (c in count) printf "%s\t%.4f\n", c, count[c]/total}') > "$output_file"
+cat scripts/fo_header.txt <(sed 's/:/\t/g' temp | awk -v OFS='\t' '{print $4,$14}' | awk -F'\t' '{count[$1] += $2; total += $2} END {for (c in count) printf "%s\t%.4f\n", c, (count[c]/total)*100}') > "$output_file"
 rm temp
+
